4f2e22dcd71b04e49ccb27f9872569ac70877d11
deweybanks <deweybanks@gmail.com>
2018-07-07 17:18:50 -0400

    adds notes to pizzabot

--- pizzabot.rb
+++ pizzabot.rb
@@ -1,8 +1,20 @@
-
+require 'pry'
 # Changed the input to a command line arugment.
 # I think this is more a long the lines what the
 # challenge was asking for
-
 command = ARGV[0]
 
-puts command
+# the command comes in as a string. I then pass a regex
+# expression to the String match method, which will return
+# any digits seperated by an x
+grid_size_string = command[/\d[x]\d/]
+
+# I use another regex with the scan method to return
+# an array everything surrounded by "()"
+coordinates = command.scan(/\(.*?\)/)
+
+# I take the array of coordinates that I get and use the
+# map method to return a new array with parentheses removed
+formatted_coordinates = coordinates.map { |i| i.gsub(/[^\d,\.]/, '') }
+
+
