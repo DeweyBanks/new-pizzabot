80840082f08ba1cffd6ecd0b3aadfca322847c38
deweybanks <deweybanks@gmail.com>
2018-07-07 18:30:47 -0400

    adds x_move

--- pizzabot.rb
+++ pizzabot.rb
@@ -14,8 +14,6 @@ command = ARGV[0]
 # command will return:
 # "5x5 (1, 3) (4, 4) (2,1)"
 
-
-
 # the command comes in as a string. I then pass a regex
 # expression to the String match method, which will return
 # any digits seperated by an x
@@ -60,3 +58,39 @@ end
 # -> [{:x_axis=>1, :y_axis=>3}, {:x_axis=>4, :y_axis=>4}, {:x_axis=>2, :y_axis=>1}]
 
 
+# The starting point is at the coordinates 0, 0. So, I create
+# an object that represents that.
+starting_location = {:x_axis => 0, :y_axis => 0}
+
+# I will take the first formatted_coordinate to set as the target_location
+target_location = formatted_coordinates.first
+
+# I create an empty array to hold the directions
+directions = []
+
+
+
+# we could get the number of moves North by the difference between this two:
+# starting_location[:x_axis] target_location[:x_axis]
+# so if SL.x_axis is 0 and TL.x_axis is 1, we need to move North one time
+
+# I set a variable to the difference of the two x_axis points
+x_move_number = target_location[:x_axis] - starting_location[:x_axis]
+# -> 1
+# here, if the number is positive it would be a move North. Negative
+# would be move South.
+
+if x_move_number > 0
+  # if it's a positive number we push "N" into the directions array
+  # times the move_number. So if move_number is 2, we push "N" into
+  # directions twice.
+  x_move_number.times { directions << "N" }
+else
+  # here the number is negative. I call .abs method on the number.
+  # this gives the absolute value of the number. Which
+  # we need to do in order to tell the .times method
+  # how many times to push "S"
+  x_move_number.abs.times { directions << "S" }
+end
+
+puts directions
